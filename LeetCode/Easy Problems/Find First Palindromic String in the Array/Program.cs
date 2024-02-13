3ad0ac086cbf28eec29241f1c0e4385e90fc41b8
Kristian Popov <kristianpopov03@yahoo.com>
2024-02-13 12:17:03 +0100

    Find First Palindromic String in the Array - C#

---
+++ LeetCode/Easy Problems/Find First Palindromic String in the Array/Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] words = { "abc", "car", "ada", "racecar", "cool" };
+
+        Console.WriteLine(FirstPalindrome(words));
+    }
+
+    private static string FirstPalindrome(string[] words)
+    {
+        ////////////////////////////////////// Variant 1 /////////////////////////////////
+        foreach (string i in words)
+            if (i == new string(i.Reverse().ToArray()))
+                return i;
+
+        return "";
+        ////////////////////////////////////// Variant 2 /////////////////////////////////
+        // return words.FirstOrDefault(x => x == string.Concat(x.Reverse())) ?? "";
+    }
+}
