509afc1d818a7f5babf300fc140c5af30611445b
Kristian Popov <kristianpopov03@yahoo.com>
2024-02-13 11:52:54 +0100

    Shuffle the Array - C#

---
+++ LeetCode/Easy Problems/Shuffle the Array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] numbers = { 2, 5, 1, 3, 4, 7 };
+        int n = 3;
+
+       int[] result = shuffle(numbers, n);
+
+        foreach (int i in result) 
+        {
+            Console.Write(i + " ");
+        }
+    }
+
+    private static int[] shuffle(int[] nums, int n)
+    {
+        int[] retArray = new int[2 * n];
+        int j = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            retArray[j] = nums[i];
+            j++;
+            retArray[j] = nums[i + n];
+            j++;
+        }
+        return retArray;
+    }
+}
