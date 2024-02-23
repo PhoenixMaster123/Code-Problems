24af15d8177d9c7b31332b7da6c6359b351ea57d
Kristian Popov <kristianpopov03@yahoo.com>
2024-02-23 16:56:31 +0100

    Combinations with Repetition

---
+++ SoftUni/Algorithms/Combinatorial Problems/Combinations with Repetition/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] elements = scanner.nextLine().split(" ");
+
+        int k = Integer.parseInt(scanner.nextLine());
+
+        String[] variations = new String[k];
+
+        combinations(0,0, variations, elements);
+    }
+
+    private static void combinations(int index, int start, String[] variations, String[] elements)
+    {
+        if(index == variations.length)
+        {
+            System.out.println(String.join(" ",variations));
+        }
+        else
+        {
+            for (int i = start; i < elements.length; i++)
+            {
+                variations[index] = elements[i];
+                combinations(index + 1, i , variations,elements); // difference
+            }
+        }
+    }
+}
