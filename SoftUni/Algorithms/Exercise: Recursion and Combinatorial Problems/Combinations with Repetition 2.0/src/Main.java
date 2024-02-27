f88917b4d0a8f9fa00cf9799162e086d9e8c7db7
Kristian Popov <kristianpopov03@yahoo.com>
2024-02-27 22:46:18 +0100

    Combinations with Repetition 2.0

---
+++ SoftUni/Algorithms/Exercise: Recursion and Combinatorial Problems/Combinations with Repetition 2.0/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int k = Integer.parseInt(scanner.nextLine());
+
+        int[] arr = new int[k];
+
+        combinations(0, 1,arr, n);
+    }
+
+    private static void combinations(int index, int start, int[] arr, int n)
+    {
+        if(index == arr.length)
+        {
+            for (int i = 0; i < arr.length; i++)
+            {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+        else
+        {
+            for (int i = start; i <= n; i++)
+            {
+                arr[index] = i;
+                combinations(index + 1, i,arr,n);
+            }
+        }
+    }
+}
