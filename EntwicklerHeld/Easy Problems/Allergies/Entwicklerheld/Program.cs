4637a7ba8f9ed315593ba1de98fa2d424ccbe019
Kristian Popov <kristianpopov03@yahoo.com>
2024-03-15 16:22:18 +0100

    Allergies - C#

---
+++ EntwicklerHeld/Easy Problems/Allergies/Entwicklerheld/Program.cs
@@ -0,0 +1,15 @@
+﻿using AllergiesCsharpImplementation;
+
+class Program
+{
+    static void Main()
+    {
+        // Example usage
+        Allergen[] allergies = { Allergen.Eggs, Allergen.Tomatoes }; // Example allergens
+        Allergies personAllergies = Allergies.CreateAllergy(allergies);
+
+        Console.WriteLine(personAllergies.IsAllergicTo(Allergen.Eggs)); // Should return true
+        Console.WriteLine(personAllergies.IsAllergicTo(Allergen.Tomatoes)); // Should return true
+        Console.WriteLine(personAllergies.IsAllergicTo(Allergen.Peanuts)); // Should return false
+    }
+}
